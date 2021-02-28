b8cf2de74a23910d79df8934e62d186d97e90b19
khayes <khayes@hothouseinc.com>
2021-02-28 14:03:41 -0500

    kotlin interfaces

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,29 @@
 fun main(args: Array<String>) {
-    val myCar = Car(name = "A3", brand = "Audi")
-    val myECar = ElectricCar(name = "S", brand = "Tesla", batteryLife = 85.0)
+    val myCar = Car(name = "A3", brand = "Audi", maxSpeed = 200.0)
+    val myECar = ElectricCar(name = "S", brand = "Tesla", batteryLife = 85.0, maxSpeed = 240.0)
 
     myECar.extendRange(200.0)
     myCar.drive(200.0)
     myECar.drive(200.0)
+
+    myCar.brake()
+    myECar.brake()
+}
+
+/*
+* INTERFACES
+* Interfaces in Kotlin can contain declarations of abstract methods,
+* as well as method implementations. What makes them different from
+* abstract classes is that interfaces cannot store state.
+* They can have properties but these need to be abstract or
+* to provide accessor implementations.
+* */
+interface Drivable {
+    val maxSpeed: Double
+    fun drive(): String
+    fun brake() {
+        println("The drivable is braking")
+    }
 }
 
 /**
@@ -20,7 +39,7 @@ open class Vehicle {
 
 // Sub class, child class of vehicle
 // Super class of electric car
-open class Car(val name: String, val brand: String) {
+open class Car(override val maxSpeed: Double, val name: String, val brand: String): Drivable {
     open var range: Double = 0.0
 
     fun extendRange(amount: Double) {
@@ -32,14 +51,23 @@ open class Car(val name: String, val brand: String) {
     open fun drive(distance: Double) {
         println("Drove for $distance KM")
     }
+
+    override fun drive(): String {
+        return "driving the interface"
+    }
 }
 
 // Sub class, child class of car
-class ElectricCar(name: String, brand: String, batteryLife: Double): Car(name, brand) {
+class ElectricCar(maxSpeed: Double, name: String, brand: String, batteryLife: Double): Car(maxSpeed, name, brand) {
     override var range = batteryLife * 6
 
     override fun drive(distance: Double) {
         super.drive(distance)
         println("Drove for $distance KM on electricity")
+    }
+
+    override fun brake() {
+        super.brake()
+        println("brake inside of electric car")
     }
 }
