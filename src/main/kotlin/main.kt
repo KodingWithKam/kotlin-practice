73fcfcc99ec962b9beda05a698f98e1b9ad603f6
khayes <khayes@hothouseinc.com>
2021-02-28 14:31:20 -0500

    kotlin arraylists

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,73 +1,37 @@
 fun main(args: Array<String>) {
-    val myCar = Car(name = "A3", brand = "Audi", maxSpeed = 200.0)
-    val myECar = ElectricCar(name = "S", brand = "Tesla", batteryLife = 85.0, maxSpeed = 240.0)
 
-    myECar.extendRange(200.0)
-    myCar.drive(200.0)
-    myECar.drive(200.0)
+    /*
+    * Arraylists are used to create a dynamic array
+    * ArrayList class provides both read/write functions
+    * ArrayList follows the sequence of insertion order
+    * ArrayList is non synchronized and it may contain dup elements
+    * */
+    val arrayList = ArrayList<String>() // Create empty array list
+    arrayList.add("One") // Add object to list
+    arrayList.add("Two")
 
-    myCar.brake()
-    myECar.brake()
+    for (i in arrayList) {
+        println(i)
+    }
+
+    println(arrayList[0])
+    println(arrayList[1])
+
+    val numList = ArrayList<Double>()
+    numList.add(2.0)
+    numList.add(4.0)
+    numList.add(6.0)
+    numList.add(8.0)
+    numList.add(10.0)
+
+    println(getAverage(numList))
 }
 
-/*
-* INTERFACES
-* Interfaces in Kotlin can contain declarations of abstract methods,
-* as well as method implementations. What makes them different from
-* abstract classes is that interfaces cannot store state.
-* They can have properties but these need to be abstract or
-* to provide accessor implementations.
-* */
-interface Drivable {
-    val maxSpeed: Double
-    fun drive(): String
-    fun brake() {
-        println("The drivable is braking")
+fun getAverage(data: ArrayList<Double>): Double {
+    var total = 0.0
+    for (i in data) {
+        total += i
     }
-}
-
-/**
- * Open keyword needs to be added to classes that you plan on using for inheritance
- */
-
-// Super class, parent class
-open class Vehicle {
-    // Properties
-
-    // methods
-}
-
-// Sub class, child class of vehicle
-// Super class of electric car
-open class Car(override val maxSpeed: Double, val name: String, val brand: String): Drivable {
-    open var range: Double = 0.0
-
-    fun extendRange(amount: Double) {
-        if(amount > 0) {
-            range += amount
-        }
-    }
-
-    open fun drive(distance: Double) {
-        println("Drove for $distance KM")
-    }
-
-    override fun drive(): String {
-        return "driving the interface"
-    }
-}
-
-// Sub class, child class of car
-class ElectricCar(maxSpeed: Double, name: String, brand: String, batteryLife: Double): Car(maxSpeed, name, brand) {
-    override var range = batteryLife * 6
-
-    override fun drive(distance: Double) {
-        super.drive(distance)
-        println("Drove for $distance KM on electricity")
-    }
-
-    override fun brake() {
-        super.brake()
-        println("brake inside of electric car")
-    }
+    println("average....")
+    return total/data.size
 }
