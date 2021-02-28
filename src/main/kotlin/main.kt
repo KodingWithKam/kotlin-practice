c117543565226b1bd964f72e32a22466f2de43cc
khayes <khayes@hothouseinc.com>
2021-02-27 20:38:18 -0500

    kotlin variables (val vs var)

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,15 @@
 fun main(args: Array<String>) {
+
+    /*
+    * Var is mutable
+    * Use Var when you want to change value later on
+    * Val is not mutable
+    * Use val when value will be const
+    */
+
+    var testVar = "Test"
+    testVar = "New Test"
+
+    val testVal = "Cant Change Value"
     println("Hello World!")
 }
